83d55f08bb870eb76d220ff6999934dc6408da39
Sandro Reis <reissandro18@gmail.com>
2026-03-30 16:25:32 +0100

    Projeto feito

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const form = document.getElementById("form");
+
+if(form){
+
+form.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+let nome = document.getElementById("nome").value;
+let email = document.getElementById("email").value;
+let mensagem = document.getElementById("mensagem").value;
+
+if(nome === "" || email === "" || mensagem === ""){
+
+alert("Preencha todos os campos");
+
+}else{
+
+alert("Mensagem enviada com sucesso!");
+
+}
+
+});
+
+}
